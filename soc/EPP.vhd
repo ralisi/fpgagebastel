80718079436f6a5304c5121f2c3c48dc245c830f
Raphael Lisicki <raphael@ralisi.de>
2016-04-01 01:35:38 +0200

    using something like registers

--- soc/EPP.vhd
+++ soc/EPP.vhd
@@ -22,6 +22,7 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use ieee.numeric_std.all;
 
  entity epp_interface is
    port (Clk     : in    std_logic;
@@ -42,24 +43,23 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
    
    signal address    : std_logic_vector(7 downto 0) := (others => '0');
    signal port0data : std_logic_vector(7 downto 0) := (others => '0');
+   subtype registerPart is std_logic_vector(7 downto 0);
+   type all_registers is array(0 to 7) of registerPart;
+   signal registers : all_registers := (X"01", X"02", X"03", X"04", X"05", X"06", X"07", X"08");
    begin   
       process(clk)
       --type   epp_state is (idle, data_read, data_read_b, data_write, data_write_b, addr_read, addr_read_b, addr_write, addr_write_b);
       type   epp_state is (idle, data_read, data_write, data_clean, addr_read, addr_write, addr_clean);
       variable state : epp_state := idle;
       variable next_state : epp_state := idle;
+      variable idx : integer range 0 to 7;
             begin
       
             if rising_edge(clk) then
                case state is
                   when data_read  =>
-                     case address is
-                        when "00000000" =>
-                           EppDB <= reg0;
-                        when "00000001" =>
-                           EppDB <= reg1;
-                        when others =>
-                     end case;
+                     idx := to_integer(unsigned(address));
+                     EppDB <= registers(idx);
                      next_state := data_clean;
                      
                   when data_write =>
