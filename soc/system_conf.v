fa3cd492d3311fe77e798753288d58ddc8b7d271
Raphael Lisicki <raphael@ralisi.de>
2016-04-02 01:21:35 +0200

    moved framebuffer to correct memory location

--- soc/system_conf.v
+++ soc/system_conf.v
@@ -8,14 +8,17 @@
 `define CFG_DISTRAM_POSEDGE_REGISTER_FILE
 `define SHIFT_ENABLE
 `define CFG_MC_BARREL_SHIFT_ENABLED
-`define LM32_I_PC_WIDTH 22
+`define LM32_I_PC_WIDTH 31
+`define ADDRESS_LOCK
 `define ebrEBR_WB_DAT_WIDTH 32
 `define ebrINIT_FILE_NAME "none"
 `define ebrINIT_FILE_FORMAT "hex"
 `define gpioGPIO_WB_DAT_WIDTH 32
 `define gpioGPIO_WB_ADR_WIDTH 4
+`define ADDRESS_LOCK
 `define gpioOUTPUT_PORTS_ONLY
 `define gpioDATA_WIDTH 32'h8
+`define ADDRESS_LOCK
 `define memory_passthruMEM_WB_DAT_WIDTH 32
 `define MEM_WB_SEL_WIDTH 4
 `define memory_passthruMEM_WB_ADR_WIDTH 32
